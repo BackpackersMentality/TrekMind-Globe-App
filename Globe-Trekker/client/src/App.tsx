2d2757b18dba36e8839d5e16e47820b6fb47aafe
BackpackersMentality <hello@backpackersmentality.com>
2026-02-12 07:21:58 -0800

    Remove Toaster component from Router
    Removed the Toaster component from the Router function.

--- Globe-Trekker/client/src/App.tsx
+++ Globe-Trekker/client/src/App.tsx
@@ -2,7 +2,6 @@ import { useMemo } from "react";
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
 import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
 
@@ -36,8 +35,7 @@ function App() {
         <div className={isEmbed ? "h-full" : "h-[calc(100vh-80px)]"}>
           <Router />
         </div>
-
-        <Toaster />
+        
       </div>
     </QueryClientProvider>
   );
